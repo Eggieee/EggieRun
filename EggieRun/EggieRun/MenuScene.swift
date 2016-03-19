bf2e14fe6b621389fec527c77fbb3c75539fe360
CNA-Bld <cna.bld@gmail.com>
2016-03-20 01:05:32 +0800

    add button sprites to `MenuScene`

--- EggieRun/EggieRun/MenuScene.swift
+++ EggieRun/EggieRun/MenuScene.swift
@@ -9,26 +9,47 @@
 import SpriteKit
 
 class MenuScene: SKScene {
+    
+    private var buttonPlay: SKSpriteNode!
+    private var buttonDex: SKSpriteNode!
+    
     override func didMoveToView(view: SKView) {
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         myLabel.text = "Eggie Run"
         myLabel.fontSize = 45
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
         self.addChild(myLabel)
         
         let label_kakkokari = SKLabelNode(fontNamed:"Chalkduster")
         label_kakkokari.text = "(name subject to change)"
         label_kakkokari.fontSize = 12
         label_kakkokari.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame)-45)
-        
         self.addChild(label_kakkokari)
+        
+        let label_temp = SKLabelNode(fontNamed:"Courier")
+        label_temp.text = "left btn -> play, right btn -> éggdex"
+        label_temp.fontSize = 12
+        label_temp.position = CGPoint(x:CGRectGetMidX(self.frame), y:20)
+        self.addChild(label_temp)
+        
+        buttonPlay = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: 160, height: 80))
+        buttonPlay.position = CGPoint(x: CGRectGetMidX(self.frame) - 120, y: 260)
+        self.addChild(buttonPlay)
+        
+        buttonDex = SKSpriteNode(color: UIColor.greenColor(), size: CGSize(width: 160, height: 80))
+        buttonDex.position = CGPoint(x: CGRectGetMidX(self.frame) + 120, y: 260)
+        self.addChild(buttonDex)
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        let gameScene = GameScene(size: self.size)
-        let transition = SKTransition.doorsOpenVerticalWithDuration(0.5)
-        self.view?.presentScene(gameScene, transition: transition)
+        let touch = touches.first!
+        let touchLocation = touch.locationInNode(self)
+        
+        if buttonPlay.containsPoint(touchLocation) {
+            let gameScene = GameScene(size: self.size)
+            let transition = SKTransition.doorsOpenVerticalWithDuration(0.5)
+            self.view?.presentScene(gameScene, transition: transition)
+        }
     }
     
     override func update(currentTime: CFTimeInterval) {
