5dd72950d226129af6b79ffaaadbb7ef69f87100
CNA-Bld <cna.bld@gmail.com>
2016-03-22 00:38:32 +0800

    implement framework of models

--- EggieRun/Dish.swift
+++ EggieRun/Dish.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 enum Dish: Int {
-    case SmashedEgg = 0, OyakoDon = 1, SurstrommingEgg = 2
+    case SmashedEgg = 0, OyakoDon = 1, SurstrommingEgg = 2, PlainBoiledEgg = -1, BlackFried = -2, BlackBaked = -3, BlackBoiled = -4
+    
+    static func getResultDish(cooker: Cooker, condiments: [Condiment], ingredients: [Ingredient]) -> Dish {
+        
+        if (cooker == .Drop) {
+            return .SmashedEgg
+        } else if (cooker == .DistanceForceDeath) {
+            return .OyakoDon
+        }
+        
+        // TODO: more dishes to come...
+        
+        if (cooker == .Pot && condiments.isEmpty) {
+            return .PlainBoiledEgg
+        } else if (cooker == .Pan) {
+            return .BlackFried
+        } else if (cooker == .Oven) {
+            return .BlackBaked
+        } else if (cooker == .Pot) {
+            return .BlackBoiled
+        }
+        
+        fatalError()
+    }
 }
---
+++ EggieRun/EggieRun/Ingredient.swift
@@ -0,0 +1,12 @@
+//
+//  Ingredient.swift
+//  EggieRun
+//
+//  Created by CNA_Bld on 3/22/16.
+//  Copyright © 2016 Eggieee. All rights reserved.
+//
+
+import Foundation
+enum Ingredient {
+    case GreenOnion, Tomato, Cream, Milk, Rice, Bread, Bacon, Strawberry, Chocolate, Surstromming
+}
