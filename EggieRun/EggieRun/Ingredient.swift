ff0cc045a66802c5621622f6ad6720490000aaf1
CNA-Bld <cna.bld@gmail.com>
2016-03-28 17:00:05 +0800

    integrate Condiment / Ingredient

--- EggieRun/EggieRun/Ingredient.swift
+++ EggieRun/EggieRun/Ingredient.swift
@@ -9,4 +9,31 @@
 import Foundation
 enum Ingredient: Int {
     case GreenOnion = 0, Tomato = 1, Cream = 2, Milk = 3, Rice = 4, Bread = 5, Bacon = 6, Strawberry = 7, Chocolate = 8, Surstromming = -1
+    
+    var next: Ingredient {
+        return .GreenOnion
+    }
+    
+    private var imageNamed: String {
+        switch self {
+        case .GreenOnion:
+            return "green-onion"
+        case .Tomato:
+            return "tomato"
+        case .Cream:
+            return "cream"
+        case .Milk:
+            return "milk"
+        default:
+            return "tomato"
+        }
+    }
+    
+    var flatImageNamed: String {
+        return "flat-" + imageNamed
+    }
+    
+    var fineImageNamed: String {
+        return "fine-" + imageNamed
+    }
 }
