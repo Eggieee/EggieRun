8ebf1d71c847651c3c06abc6d963384de1921e64
liuyang <melody9951213@gmail.com>
2016-03-28 17:43:29 +0800

    refactored collectable factory

--- EggieRun/EggieRun/Collectable.swift
+++ EggieRun/EggieRun/Collectable.swift
@@ -9,6 +9,8 @@
 import SpriteKit
 
 class Collectable: SKSpriteNode {
+    private static let SIZE = CGSizeMake(80, 80)
+    
     let type: CollectableType
     let ingredient: Ingredient?
     let condiment: Condiment?
@@ -20,14 +22,8 @@ class Collectable: SKSpriteNode {
         type = .Ingredient
         followingGapSize = gapSize
         let texture = SKTexture(imageNamed: ingredient!.fineImageNamed)
-        let size = CGSizeMake(80, 80)
-        super.init(texture: texture, color: UIColor.clearColor(), size: size)
-        
-        physicsBody = SKPhysicsBody(rectangleOfSize: size)
-        physicsBody?.categoryBitMask = BitMaskCategory.collectable
-        physicsBody?.contactTestBitMask = BitMaskCategory.hero
-        physicsBody?.dynamic = false
-        zPosition = 1
+        super.init(texture: texture, color: UIColor.clearColor(), size: Collectable.SIZE)
+        initializePhysicsProperty()
     }
     
     init(condimentType: Condiment, gapSize: CGFloat) {
@@ -36,17 +32,19 @@ class Collectable: SKSpriteNode {
         type = .Condiment
         followingGapSize = gapSize
         let texture = SKTexture(imageNamed: condiment!.imageNamed)
-        let size = CGSizeMake(80, 80)
-        super.init(texture: texture, color: UIColor.clearColor(), size: size)
-        
+        super.init(texture: texture, color: UIColor.clearColor(), size: Collectable.SIZE)
+        initializePhysicsProperty()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initializePhysicsProperty() {
         physicsBody = SKPhysicsBody(rectangleOfSize: size)
         physicsBody?.categoryBitMask = BitMaskCategory.collectable
         physicsBody?.contactTestBitMask = BitMaskCategory.hero
         physicsBody?.dynamic = false
         zPosition = 1
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
