25862ec42b61ccd1d1bd49d55f20e9f321e6542c
onizudango <onizudango@gmail.com>
2016-03-31 22:32:59 +0800

    add in ingredient grid img

--- EggieRun/EggieRun/IngredientBar.swift
+++ EggieRun/EggieRun/IngredientBar.swift
@@ -9,5 +9,12 @@
 import SpriteKit
 
 class IngredientBar: SKSpriteNode {
+    init(position: CGPoint) {
+        let barSize = CGSize(width: 500, height: 90)
+        super.init(texture: nil, color: UIColor.clearColor(), size: barSize)
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
---
+++ EggieRun/EggieRun/IngredientGrid.swift
@@ -0,0 +1,13 @@
+//
+//  IngredientGrid.swift
+//  EggieRun
+//
+//  Created by  light on 2016/03/31.
+//  Copyright © 2016年 Eggieee. All rights reserved.
+//
+
+import SpriteKit
+
+class IngredientGrid: SKSpriteNode {
+    
+}
