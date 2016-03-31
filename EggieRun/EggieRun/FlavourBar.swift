7a7aa49be36342a5e7e4987c8dee5a9638994fc4
onizudango <onizudango@gmail.com>
2016-03-31 22:06:25 +0800

    Add in flavour bar & ingredient bar file

---
+++ EggieRun/EggieRun/FlavourBar.swift
@@ -0,0 +1,13 @@
+//
+//  FlavourBar.swift
+//  EggieRun
+//
+//  Created by  light on 2016/03/31.
+//  Copyright © 2016年 Eggieee. All rights reserved.
+//
+
+import SpriteKit
+
+class FlavourBar: SKSpriteNode {
+    
+}
---
+++ EggieRun/EggieRun/IngredientBar.swift
@@ -0,0 +1,13 @@
+//
+//  IngredientBar.swift
+//  EggieRun
+//
+//  Created by  light on 2016/03/31.
+//  Copyright © 2016年 Eggieee. All rights reserved.
+//
+
+import SpriteKit
+
+class IngredientBar: SKSpriteNode {
+    
+}
