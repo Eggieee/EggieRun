19ed53540ceed6dea538a61524af554118956b85
onizudango <onizudango@gmail.com>
2016-04-02 17:26:49 +0800

    update ingredient bar

--- EggieRun/EggieRun/IngredientBar.swift
+++ EggieRun/EggieRun/IngredientBar.swift
@@ -9,8 +9,21 @@
 import SpriteKit
 
 class IngredientBar: SKSpriteNode {
+    private static let X_DISTANCE = CGFloat(100)
+    private static let MAX_GRID_NUMBER = 5
+    
     var ingredients = [Ingredient]()
     var ingredientGrids = [IngredientGrid]()
+    var firstEmptyIndex: Int {
+        get {
+            return ingredients.count
+        }
+    }
+    var isFull: Bool {
+        get {
+            return (firstEmptyIndex >= IngredientBar.MAX_GRID_NUMBER)
+        }
+    }
     
     init(position: CGPoint) {
         let barSize = CGSizeMake(500, 90)
@@ -20,6 +33,50 @@ class IngredientBar: SKSpriteNode {
     // todo
     func addIngredient(newIngredient: Ingredient) {
         
+    }
+    
+    func updateArray(newIngredient: Ingredient, newGrid: IngredientGrid) {
+        if (isFull) {
+            moveGridByOne()
+        }
+        ingredients[firstEmptyIndex] = newIngredient
+        ingredientGrids[firstEmptyIndex] = newGrid
+    }
+    
+    // new grid is added into array alr
+    func updateBarLayout(newGrid: IngredientGrid) {
+        if (isFull) {
+            animateMovingGridByOne()
+        }
+        animateAddingNewGrid(newGrid)
+    }
+    
+    func animateMovingGridByOne() {
+        for i in 0..<ingredients.count {
+            let grid = ingredientGrids[i]
+            // moving animation goes here
+            grid.position.x -= IngredientBar.X_DISTANCE
+            
+            if (i==0) {
+                grid.hidden = true
+                grid.removeFromParent()
+            }
+        }
+    }
+    
+    func animateAddingNewGrid(newGrid: IngredientGrid) {
+        let position = CGPointMake(CGFloat(firstEmptyIndex) * IngredientBar.X_DISTANCE, 0)
+        newGrid.position = position
+        addChild(newGrid)
+    }
+    
+    func moveGridByOne() {
+        for i in 0..<IngredientBar.MAX_GRID_NUMBER-1 {
+            ingredients[i] = ingredients[i+1]
+            ingredientGrids[i] = ingredientGrids[i+1]
+        }
+        ingredients.removeLast()
+        ingredientGrids.removeLast()
     }
     
     required init?(coder aDecoder: NSCoder) {
