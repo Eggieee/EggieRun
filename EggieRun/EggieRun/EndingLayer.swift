3528bcd70f0ba36a435188e5c4b1a8521b068b85
onizudango <onizudango@gmail.com>
2016-04-08 16:59:03 +0800

    add in beam animation for ending screen

--- EggieRun/EggieRun/EndingLayer.swift
+++ EggieRun/EggieRun/EndingLayer.swift
@@ -11,11 +11,19 @@ import SpriteKit
 class EndingLayer: SKSpriteNode {
     private var dish: Dish
     private var background: SKSpriteNode!
+    private var beam: SKSpriteNode!
+    private var beamAction: SKAction
     
     init(generatedDish: Dish) {
+        let beamAtlas = SKTextureAtlas(named: "beam.atlas")
+        let sortedBeamTextureNames = beamAtlas.textureNames.sort()
+        let beamTextures = sortedBeamTextureNames.map({ beamAtlas.textureNamed($0) })
+        beamAction = SKAction.repeatActionForever(SKAction.animateWithTextures(beamTextures, timePerFrame: 0.005))
+        
         dish = generatedDish
         super.init(texture: nil, color: UIColor.clearColor(), size: UIScreen.mainScreen().bounds.size)
         fadeInBackground()
+        displayDish()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -25,8 +33,24 @@ class EndingLayer: SKSpriteNode {
     private func fadeInBackground() {
         background = SKSpriteNode(imageNamed: "ending-background")
         background.alpha = 0
+        background.zPosition = 0
         addChild(background)
-        let fadeInAction = SKAction.fadeInWithDuration(1.0)
+        let fadeInAction = SKAction.fadeInWithDuration(0.5)
         background.runAction(fadeInAction)
+    }
+    
+    private func displayDish() {
+        let dishImage = SKSpriteNode(imageNamed: "smashed-egg")
+        let scaleUpAction = SKAction.scaleTo(1, duration: 0.5)
+        dishImage.setScale(0)
+        dishImage.zPosition = 2
+        addChild(dishImage)
+        dishImage.alpha = 1
+        dishImage.runAction(scaleUpAction, completion: { () -> Void in
+            self.beam = SKSpriteNode(imageNamed: "ending-beam-1")
+            self.beam.zPosition = 1
+            self.addChild(self.beam)
+            self.beam.runAction(self.beamAction)
+        })
     }
 }
