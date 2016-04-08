7abbaa6291b055bf1af548d95e2c1c51186092ed
onizudango <onizudango@gmail.com>
2016-04-08 11:44:51 +0800

    update ending image & ending layer

---
+++ EggieRun/EggieRun/EndingLayer.swift
@@ -0,0 +1,13 @@
+//
+//  EndingLayer.swift
+//  EggieRun
+//
+//  Created by  light on 2016/04/08.
+//  Copyright © 2016年 Eggieee. All rights reserved.
+//
+
+import SpriteKit
+
+class EndingLayer: SKSpriteNode {
+    
+}
