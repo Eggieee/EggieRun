71338431f136c947a28847f90356a5f3578a3d87
liuyang <melody9951213@gmail.com>
2016-04-17 16:59:07 +0800

    implemented pan

--- EggieRun/EggieRun/RunningManager/Obstacle.swift
+++ EggieRun/EggieRun/RunningManager/Obstacle.swift
@@ -10,62 +10,16 @@ import SpriteKit
 
 class Obstacle: SKNode {
     static let WIDTH = 200.0
-    private static let OVEN_PADDING: CGFloat = -27.0
-    private static let POT_PADDING: CGFloat = -20.0
-    private static let ATLAS_TIME_PER_FRAME = 0.05
+    static let ATLAS_TIME_PER_FRAME = 0.05
     
-    let cookerType: Cooker
+    let cookerType: Cooker!
     var isPassed = false
     var heightPadding: CGFloat = 0.0
-    private let baseNode: SKSpriteNode
-    private var assistingNode: SKSpriteNode? = nil
     
     init(cooker: Cooker) {
         cookerType = cooker
-        
-        switch cooker {
-        case .Oven:
-            baseNode = SKSpriteNode(imageNamed: "oven-open")
-            heightPadding = Obstacle.OVEN_PADDING
-        case .Pan:
-            baseNode = SKSpriteNode(imageNamed: "pan")
-        case .Pot:
-            baseNode = SKSpriteNode(imageNamed: "pot-body")
-            assistingNode = SKSpriteNode(imageNamed: "pot-lid")
-            heightPadding = Obstacle.POT_PADDING
-        default:
-            fatalError()
-        }
-        
         super.init()
         zPosition = 2
-
-        scaleNode(baseNode, width: Obstacle.WIDTH)
-        baseNode.position.x = baseNode.size.width / 2
-        baseNode.position.y = baseNode.size.height / 2
-        addChild(baseNode)
-        
-        if cooker == .Pot {
-            scaleNode(assistingNode!, width: Obstacle.WIDTH)
-            
-            assistingNode!.position.x = assistingNode!.size.width / 2
-            assistingNode!.position.y = assistingNode!.size.height / 2 + 80
-            addChild(assistingNode!)
-            
-            assistingNode!.physicsBody = SKPhysicsBody(rectangleOfSize: assistingNode!.size)
-            assistingNode!.physicsBody!.categoryBitMask = BitMaskCategory.obstacle
-            assistingNode!.physicsBody!.contactTestBitMask = BitMaskCategory.hero
-            assistingNode!.physicsBody!.collisionBitMask = BitMaskCategory.hero | BitMaskCategory.obstacle
-            assistingNode!.physicsBody!.dynamic = false
-        }
-        
-        if cooker != .Pan {
-            baseNode.physicsBody = SKPhysicsBody(rectangleOfSize: baseNode.size)
-            baseNode.physicsBody!.categoryBitMask = BitMaskCategory.obstacle
-            baseNode.physicsBody!.contactTestBitMask = BitMaskCategory.hero
-            baseNode.physicsBody!.collisionBitMask = BitMaskCategory.hero | BitMaskCategory.obstacle
-            baseNode.physicsBody!.dynamic = false
-        }
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -73,28 +27,10 @@ class Obstacle: SKNode {
     }
     
     func isDeadly(vector: CGVector, point: CGPoint) -> Bool {
-        switch cookerType {
-        case .Oven:
-            return vector.dx != 0
-        case .Pan:
-            return false
-        default:
-            return nodeAtPoint(point) == baseNode || vector.dx != 0
-        }
+        fatalError()
     }
     
     func animateClose() {
-        if cookerType == .Oven {
-            let atlas = SKTextureAtlas(named: "oven-close.atlas")
-            let textures = atlas.textureNames.sort().map({ atlas.textureNamed($0) })
-            baseNode.runAction(SKAction.repeatActionForever(SKAction.animateWithTextures(textures, timePerFrame: Obstacle.ATLAS_TIME_PER_FRAME)))
-        } else if cookerType == .Pot {
-            assistingNode!.physicsBody!.dynamic = true
-        }
-    }
-    
-    private func scaleNode(node: SKSpriteNode, width: Double) {
-        let aspectRatio = Double(node.size.height / node.size.width)
-        node.size = CGSize(width: width, height: width * aspectRatio)
+        fatalError()
     }
 }
--- EggieRun/EggieRun/RunningManager/Pan.swift
+++ EggieRun/EggieRun/RunningManager/Pan.swift
@@ -1,5 +1,5 @@
 //
-//  Pot.swift
+//  Pan.swift
 //  EggieRun
 //
 //  Created by Liu Yang on 17/4/16.
@@ -8,18 +8,16 @@
 
 import SpriteKit
 
-class Pot: Obstacle {
-    private static let PADDING: CGFloat = -20
+class Pan: Obstacle {
     private static let LID_HEIGHT: CGFloat = 80
     
     private var body: SKSpriteNode!
-    private var lid: SKSpriteNode!
-
+    private var lid: SKSpriteNode?
+    
     init() {
-        super.init(cooker: .Pot)
-        heightPadding = Pot.PADDING
-
-        body = SKSpriteNode(imageNamed: "pot-body")
+        super.init(cooker: .Pan)
+        
+        body = SKSpriteNode(imageNamed: "pan")
         body.scale(Obstacle.WIDTH)
         body.position.x = body.size.width / 2
         body.position.y = body.size.height / 2
@@ -30,27 +28,27 @@ class Pot: Obstacle {
         body.physicsBody!.dynamic = false
         addChild(body)
         
-        lid = SKSpriteNode(imageNamed: "pot-lid")
-        lid.scale(Obstacle.WIDTH)
-        lid!.position.x = lid!.size.width / 2
-        lid!.position.y = lid!.size.height / 2 + Pot.LID_HEIGHT
-        lid!.physicsBody = SKPhysicsBody(rectangleOfSize: lid!.size)
-        lid!.physicsBody!.categoryBitMask = BitMaskCategory.obstacle
-        lid!.physicsBody!.contactTestBitMask = BitMaskCategory.hero
-        lid!.physicsBody!.collisionBitMask = BitMaskCategory.hero | BitMaskCategory.obstacle
-        lid!.physicsBody!.dynamic = false
-        addChild(lid!)
+//        lid = SKSpriteNode(imageNamed: "pot-lid")
+//        lid.scale(Obstacle.WIDTH)
+//        lid!.position.x = lid!.size.width / 2
+//        lid!.position.y = lid!.size.height / 2 + Pot.LID_HEIGHT
+//        lid!.physicsBody = SKPhysicsBody(rectangleOfSize: lid!.size)
+//        lid!.physicsBody!.categoryBitMask = BitMaskCategory.obstacle
+//        lid!.physicsBody!.contactTestBitMask = BitMaskCategory.hero
+//        lid!.physicsBody!.collisionBitMask = BitMaskCategory.hero | BitMaskCategory.obstacle
+//        lid!.physicsBody!.dynamic = false
+//        addChild(lid!)
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     override func isDeadly(vector: CGVector, point: CGPoint) -> Bool {
-        return nodeAtPoint(point) == body || vector.dx != 0
+        return false
     }
     
     override func animateClose() {
-        lid!.physicsBody!.dynamic = true
+        
     }
 }
