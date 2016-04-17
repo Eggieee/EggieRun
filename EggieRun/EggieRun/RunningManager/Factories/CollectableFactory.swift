4140ab188407401782c8777648f0f4f4e1962ed9
liuyang <melody9951213@gmail.com>
2016-04-18 02:29:42 +0800

    adjusted parameters

--- EggieRun/EggieRun/RunningManager/Factories/CollectableFactory.swift
+++ EggieRun/EggieRun/RunningManager/Factories/CollectableFactory.swift
@@ -10,13 +10,12 @@ import UIKit
 
 class CollectableFactory {
     private static let MAX_NUM_OF_MID_PIECE: UInt32 = 2
-    private static let MAX_NUM_OF_GAP: UInt32 = 20
-    private static let MIN_NUM_OF_GAP: UInt32 = 1
-    private static let UNIT_GAP_SIZE: CGFloat = 100
+    private static let MAX_NUM_OF_GAP: UInt32 = 5
+    private static let UNIT_GAP_SIZE: CGFloat = 200
     
     func nextColletable() -> Collectable {
         let random = arc4random() % 6
-        let numOfGap = CGFloat(arc4random() % (CollectableFactory.MAX_NUM_OF_GAP - CollectableFactory.MIN_NUM_OF_GAP + 1) + CollectableFactory.MIN_NUM_OF_GAP)
+        let numOfGap = CGFloat(arc4random() % CollectableFactory.MAX_NUM_OF_GAP + 1)
         let gapSize = numOfGap * CollectableFactory.UNIT_GAP_SIZE
 
         return random == 0 ? Collectable(condimentType: Condiment.next(), gapSize: gapSize) : Collectable(ingredientType: Ingredient.next(100), gapSize: gapSize)
