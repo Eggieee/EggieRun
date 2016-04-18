7ec44d2e84873cb210946ac3abbdf778b54b31c5
onizudango <onizudango@gmail.com>
2016-04-18 14:03:55 +0800

    add in milestone bubble

--- EggieRun/EggieRun/RunningProgressBar.swift
+++ EggieRun/EggieRun/RunningProgressBar.swift
@@ -13,19 +13,23 @@ import SpriteKit
 class RunningProgressBar: SKSpriteNode {
     private static let BAR_HEIGHT: CGFloat = 10
     private static let MAX_DISTANCE = 100000
+    private static let BUBBLE_Y: CGFloat = 0.0
     
     private var barLength: CGFloat
     private var distance = 0
     private var distanceBar: SKSpriteNode!
     private let milestones: [Milestone]
+    private var nextMilestone: Milestone
     
     init(length: CGFloat, allMilestones: [Milestone]) {
         barLength = length
         milestones = allMilestones
+        nextMilestone = milestones[0]
         super.init(texture: nil, color: UIColor.grayColor(), size: CGSizeMake(barLength, RunningProgressBar.BAR_HEIGHT))
         anchorPoint.x = 0
         anchorPoint.y = 1
         initializeBar()
+        initializeMilestones()
     }
     
     private func initializeBar() {
@@ -33,6 +37,16 @@ class RunningProgressBar: SKSpriteNode {
         distanceBar.anchorPoint.x = 0
         distanceBar.anchorPoint.y = 1
         addChild(distanceBar)
+    }
+    
+    private func initializeMilestones() {
+        for milestone in milestones {
+            //let bubbleTexture = SKTexture(imageNamed: "mono-milestone-template")
+            let milestoneBubble = SKSpriteNode(imageNamed: "mono-milestone-template")
+            let xPosition = CGFloat(milestone.requiredDistance) / CGFloat(RunningProgressBar.MAX_DISTANCE) * barLength
+            milestoneBubble.position = CGPointMake(xPosition, RunningProgressBar.BUBBLE_Y)
+            addChild(milestoneBubble)
+        }
     }
 
     func updateDistance(movedDistance: Double) {
