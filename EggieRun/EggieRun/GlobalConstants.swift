07dfaf49b444ba9759ebb392597767860a28ee61
liuyang <melody9951213@gmail.com>
2016-04-18 09:47:24 +0800

    adjusted acceleration and shelf height

--- EggieRun/EggieRun/GlobalConstants.swift
+++ EggieRun/EggieRun/GlobalConstants.swift
@@ -11,6 +11,6 @@ import UIKit
 struct GlobalConstants {
     static let fontName = "Chalkduster"
     static let PHYSICS_BODY_ALPHA_THRESHOLD: Float = 0.9
-    static let EGGIE_JUMPING_ACCELERATION = CGVectorMake(0, 1500)
+    static let EGGIE_JUMPING_ACCELERATION = CGVectorMake(0, 1000)
     static let GRAVITY = CGVectorMake(0, -30)
 }
