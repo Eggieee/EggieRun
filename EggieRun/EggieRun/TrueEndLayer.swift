cd5cf8e764b67eb16fc81005ca4eea0313438c09
liuyang <melody9951213@gmail.com>
2016-04-19 22:06:33 +0800

    change true end animation

--- EggieRun/EggieRun/TrueEndLayer.swift
+++ EggieRun/EggieRun/TrueEndLayer.swift
@@ -11,17 +11,19 @@ import SpriteKit
 class TrueEndLayer: SKNode {
     
     private static let BACKGROUND_Z_POSITION: CGFloat = 1
-    private static let EGGIE_Z_POSITION: CGFloat = 2
+    private static let EGGIE_CHICKIE_Z_POSITION: CGFloat = 2
     private static let FILTER_Z_POSITION: CGFloat = 3
     private static let ALPHA: CGFloat = 0
     private static let FADE_TIME = 0.25
     private static let HIGHLIGHT_FILTER = CIFilter(name: "CIColorControls", withInputParameters: ["inputBrightness": 1])
     private static let ATLAS_TIME_PER_FRAME = 0.25
-    private static let REPEAT = 5
     private static let NAME_EGGIE = "eggie"
+    private static let NAME_CHICKIE = "chickie"
     private static let NAME_FILTER = "filter"
     private static let TEXTURE_EGGIE = SKTexture(imageNamed: "stand")
     private static let TEXTURE_CHICKIE = SKTexture(imageNamed: "oven-open")
+    private static let ENLARGE_ACTION = SKAction.resizeToWidth(250, height: 250, duration: TrueEndLayer.ATLAS_TIME_PER_FRAME)
+    private static let SHRINK_ACTION = SKAction.resizeToWidth(50, height: 50, duration: TrueEndLayer.ATLAS_TIME_PER_FRAME)
 
     private var action: SKAction!
     
@@ -42,21 +44,26 @@ class TrueEndLayer: SKNode {
         effectNode.name = TrueEndLayer.NAME_FILTER
 
         let eggie = SKSpriteNode(texture: TrueEndLayer.TEXTURE_EGGIE)
-        eggie.zPosition = TrueEndLayer.EGGIE_Z_POSITION
+        eggie.zPosition = TrueEndLayer.EGGIE_CHICKIE_Z_POSITION
         eggie.position = CGPointMake(frame.midX, frame.midY)
         eggie.name = TrueEndLayer.NAME_EGGIE
+        
+        let chickie = SKSpriteNode(texture: TrueEndLayer.TEXTURE_CHICKIE)
+        chickie.zPosition = TrueEndLayer.EGGIE_CHICKIE_Z_POSITION
+        chickie.position = CGPointMake(frame.midX, frame.midY)
+        chickie.name = TrueEndLayer.NAME_CHICKIE
 
         effectNode.addChild(eggie)
+        effectNode.addChild(chickie)
         addChild(effectNode)
         
         let fadeInAction = SKAction.fadeInWithDuration(TrueEndLayer.FADE_TIME)
-        var textures = [SKTexture]()
-        textures.append(TrueEndLayer.TEXTURE_EGGIE)
-        textures.append(TrueEndLayer.TEXTURE_CHICKIE)
+        let eggieAction = SKAction.repeatActionForever(SKAction.sequence([TrueEndLayer.ENLARGE_ACTION, TrueEndLayer.SHRINK_ACTION]))
+        let chickieAction = SKAction.repeatActionForever(SKAction.sequence([TrueEndLayer.SHRINK_ACTION, TrueEndLayer.ENLARGE_ACTION]))
+        let eggieActions = SKAction.runAction(SKAction.runAction(eggieAction, onChildWithName: TrueEndLayer.NAME_EGGIE), onChildWithName: TrueEndLayer.NAME_FILTER)
+        let chickieActions = SKAction.runAction(SKAction.runAction(chickieAction, onChildWithName: TrueEndLayer.NAME_CHICKIE), onChildWithName: TrueEndLayer.NAME_FILTER)
 
-        let envolveAction = SKAction.runAction(SKAction.runAction(SKAction.repeatActionForever(SKAction.animateWithTextures(textures, timePerFrame: TrueEndLayer.ATLAS_TIME_PER_FRAME)), onChildWithName: TrueEndLayer.NAME_EGGIE), onChildWithName: TrueEndLayer.NAME_FILTER)
-        
-        action = SKAction.sequence([fadeInAction, envolveAction])
+        action = SKAction.sequence([fadeInAction, SKAction.group([eggieActions, chickieActions])])
     }
     
     func animate() {
